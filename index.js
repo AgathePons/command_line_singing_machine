bc108f241ebc26b16ee69bcd93f94559c7419568
AgathePons <ponsagathe37@gmail.com>
2021-12-01 16:04:26 +0100

    done without bonus

--- index.js
+++ index.js
@@ -16,3 +16,44 @@ const hitParade = [
 
 
 // Votre code va ici
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const answerPgrm = [
+    'Je m\'appelle Charlu, je m\'appelle Lili, vous êtes chez O\'clock',
+    hitParade,
+    'quitter',
+];
+const answerCommands = ['chante', 'classement', 'quitter'];
+
+
+const hitMochinePgrm = () => {
+    //je pose la question
+    rl.question('Que souhaitez-vous ?', (userAnswer) => {
+        if (userAnswer === answerCommands[0]) {
+            console.log(answerPgrm[0]);
+            hitMochinePgrm();
+        } else if (userAnswer === answerCommands[1]) {
+            console.log('print classement');
+            displayClassement();
+            hitMochinePgrm();
+        } else if (userAnswer === answerCommands[2]) {
+            console.log('quit');
+            rl.close();
+        } else {
+            console.log('J\'ai mal compris...');
+            hitMochinePgrm();
+        }
+    });
+};
+
+const displayClassement = () => {
+    for (const index in hitParade) {
+        console.log(`${index} - ${hitParade[index]}`);
+    }
+}
+
+hitMochinePgrm();
